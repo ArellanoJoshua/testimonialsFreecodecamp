1af1f15b12d44f09c4d69a930979a5da48c529b3
Joshua Arellano <josharel123@gmail.com>
2022-04-17 10:53:07 -0500

    minor corrections

--- src/components/testimony.js
+++ src/components/testimony.js
@@ -9,8 +9,10 @@ function Testimony(props) {
         src={require(`../images/testimony-${props.image}.png`)} 
         alt='emmas photography' />
       <div className='container-text-testimony'>
-        <p className='name-testimony'>{props.name} in {props.country}</p>
-        <p className='position-testimony'>{props.position} at {props.company}</p>
+        <p className='name-testimony'>
+          <strong>{props.name}</strong> in {props.country}</p>
+        <p className='position-testimony'>
+          {props.position} at <strong>{props.company}</strong></p>
         <p className='text-testimony'>"{props.testimony}"</p>
       </div>
     </div>
